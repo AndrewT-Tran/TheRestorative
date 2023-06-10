613e04d668d016a92c5a8285f637e3f4a55f71a5
Andrew T <me@andrewtran-shi.dev>
2023-06-10 00:40:19 -0700

    new file:   assets/favicon.ico 	new file:   assets/img/erik-mclean--iJgjj33eEk-unsplash_450x300.jpg 	new file:   assets/img/mnz-ToLMORRb97Q-unsplash_450x300.jpg new file:   assets/img/shirts450x300.jpg 	new file:   assets/img/shirts450x300v2.jpg 	new file:   assets/img/stock600x700.jpg 	new file:   css/styles.css 	new file:   favicon.ico 	new file:   img/erik-mclean--iJgjj33eEk-unsplash_450x300.jpg 	new file:   img/mnz-ToLMORRb97Q-unsplash_450x300.jpg 	new file:   img/shirts450x300.jpg 	new file:   img/shirts450x300v2.jpg 	new file:   img/stock600x700.jpg 	new file:   index.html 	new file:   js/scripts.js 	new file:   scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+/*!
+* Start Bootstrap - Shop Item v5.0.6 (https://startbootstrap.com/template/shop-item)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+// Get the quantity input element
+// const quantityInput = document.getElementById("quantity");
+
+// // Get the current quantity
+// const currentQuantity = parseInt(quantityInput.value, 10);
+
+// // Update the quantity
+// quantityInput.value = currentQuantity + 1;
+
+// var likes = document.querySelector('#likes2')
+// var count = document.querySelector("#likes");
+// var count = document.querySelector("#likes")
+
+// function add1(id){
+// var myCart = document.querySelector(id)
+// myCart.innerText++
+// }
+// console.log()
+
+// function myFunction() {
+//     document.getElementById("myNumber").stepUp(1);
+//   }
+
+var likes = document.querySelector('#likes2')
+var count = document.querySelector("#likes");
+var count = document.querySelector("#likes")
+
+function add1(id){
+var likes = document.querySelector(id)
+likes.innerText++
+}x
+console.log()
